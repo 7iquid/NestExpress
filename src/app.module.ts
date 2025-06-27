b4d0d8283cf6fc14951c283d7049fe113145abb6
Tavie <taviefalcon@yahoo.com>
2025-06-27 16:42:54 +0800

    up @nestjs/throttler@latest

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { AgenciesModule } from './agencies/agencies.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
       ttl: 60 * 1000, // 1 minute
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 10,
+          ttl: 60,
+        },
+      ],
     }),
     AgenciesModule,
     AuthModule,

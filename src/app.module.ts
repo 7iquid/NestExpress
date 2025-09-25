0b7432de99d4ee4173b649be8e02eaaa22e726cf
tavie <taviefalcon@yahoo.com>
2025-09-25 18:15:32 +0800

    chore: refactor folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AgenciesModule } from './agencies/agencies.module';
+import { AgenciesModule } from './modules/agencies/agencies.module';
 import { CacheModule } from '@nestjs/cache-manager';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseModule } from './firebase/firebase.module';
